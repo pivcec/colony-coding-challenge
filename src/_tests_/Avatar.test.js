523c10e1d752e669bde02333d4797dc3e89ab16c
circuit-monkey <p.ivcec@gmail.com>
2018-04-29 16:07:16 +0200

    develop tests

--- src/_tests_/Avatar.test.js
+++ src/_tests_/Avatar.test.js
@@ -35,4 +35,52 @@ describe('Renders...', () => {
     const avatarImage = avatar.find(AvatarImage);
     expect(avatarImage.instance()).toBeInstanceOf(React.Component);
   });
+
+  it('Renders correct style when avatar image is not active, is hovered and is not selected', () => {
+    const updateHoveredAvatarId = jest.fn();
+    const updateSelectedAvatarId = jest.fn();
+    const avatar = mount(<Avatar
+      activeAvatarId={null}
+      hoveredAvatarId={1}
+      id={1}
+      selectedAvatarId={2}
+      updateHoveredAvatarId={() => updateHoveredAvatarId()}
+      updateSelectedAvatarId={() => updateSelectedAvatarId()}
+    />);
+    const avatarImage = avatar.find(AvatarImage);
+    const avatarImageStyle = avatarImage.prop('style');
+    expect(avatarImageStyle).toHaveProperty('border', '3px solid rgb(155, 160, 163)');
+  });
+
+  it('Renders correct style when avatar image is not active, is hovered and is selected', () => {
+    const updateHoveredAvatarId = jest.fn();
+    const updateSelectedAvatarId = jest.fn();
+    const avatar = mount(<Avatar
+      activeAvatarId={null}
+      hoveredAvatarId={1}
+      id={1}
+      selectedAvatarId={1}
+      updateHoveredAvatarId={() => updateHoveredAvatarId()}
+      updateSelectedAvatarId={() => updateSelectedAvatarId()}
+    />);
+    const avatarImage = avatar.find(AvatarImage);
+    const avatarImageStyle = avatarImage.prop('style');
+    expect(avatarImageStyle).toHaveProperty('border', '3px solid rgb(122, 161, 178)');
+  });
+
+  it('Renders correct style when avatar image is not active, is not hovered and is selected', () => {
+    const updateHoveredAvatarId = jest.fn();
+    const updateSelectedAvatarId = jest.fn();
+    const avatar = mount(<Avatar
+      activeAvatarId={null}
+      hoveredAvatarId={null}
+      id={1}
+      selectedAvatarId={1}
+      updateHoveredAvatarId={() => updateHoveredAvatarId()}
+      updateSelectedAvatarId={() => updateSelectedAvatarId()}
+    />);
+    const avatarImage = avatar.find(AvatarImage);
+    const avatarImageStyle = avatarImage.prop('style');
+    expect(avatarImageStyle).toHaveProperty('border', '3px solid rgb(122, 161, 178)');
+  });
 });
